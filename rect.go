bb6e07f90cd6db423303128bb54da5d727510b28
Viktor Ogeman <viktor.ogeman@gmail.com>
2020-05-03 12:27:42 +0200

    Rect: Add W(), H() convenience functions

--- rect.go
+++ rect.go
@@ -4,3 +4,13 @@ package v2d
 func (r Rect) Eq(q Rect, tol float32) bool {
 	return r.Min.Eq(q.Min, tol) && r.Max.Eq(q.Max, tol)
 }
+
+// W returns the width (x dimension) of the rectangle.
+func (r Rect) W() float32 {
+	return r.Max.X - r.Min.X
+}
+
+// H returns the height (y dimension) of the rectangle.
+func (r Rect) H() float32 {
+	return r.Max.Y - r.Min.Y
+}
