cdd34929c4fa1a5b9730105c1a22721119cb1c5f
Viktor Ogeman <viktor.ogeman@gmail.com>
2020-05-07 20:38:51 +0200

    Add methods to access corners of rect

--- rect.go
+++ rect.go
@@ -14,3 +14,25 @@ func (r Rect) W() float32 {
 func (r Rect) H() float32 {
 	return r.Max.Y - r.Min.Y
 }
+
+// C0 returns the first corner of the rectangle.
+func (r Rect) C0() Vec {
+	return r.Min
+}
+
+// C1 returns the second corner of the rectangle.
+func (r Rect) C1() Vec {
+	r.Min.X = r.Max.X
+	return r.Min
+}
+
+// C2 returns the third corner of the rectangle.
+func (r Rect) C2() Vec {
+	return r.Max
+}
+
+// C3 returns the forth corner of the rectangle.
+func (r Rect) C3() Vec {
+	r.Min.Y = r.Max.Y
+	return r.Min
+}
